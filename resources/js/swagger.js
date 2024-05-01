5cc27d5ab0bd0cf1bd7afd4d5466d97c48dbd336
admin <this.mail.for.mail@gmail.com>
2024-05-01 21:04:36 +0300

    правки в документации

--- resources/js/swagger.js
+++ resources/js/swagger.js
@@ -1,9 +1,18 @@
-import SwaggerUI from "swagger-ui"
+import SwaggerUI from "swagger-ui";
 import "swagger-ui/dist/swagger-ui.css";
 
 SwaggerUI({
     dom_id: "#swagger-api",
     url: "/api-docs/v1.json",
     persistAuthorization: true,
-    docExpansion: "none", 
+    docExpansion: "none",
+
+    requestInterceptor: (req) => {
+        const authorized = JSON.parse(localStorage.getItem("authorized"));
+
+        if (authorized?.AccessToken)
+            req.headers.Authorization = "Bearer " + authorized.AccessToken.value;
+
+        return req;
+    },
 });
